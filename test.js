c3b4f1048d42739c03f5160157a1401f5e9b4927
wuct <wu.chingting@gmail.com>
2016-03-26 15:04:31 +0800

    test(rafThrottle): add more tests

--- test.js
+++ test.js
@@ -1,17 +1,54 @@
 import test from 'ava'
+import { spy } from 'sinon'
 import raf from 'raf'
 import throttle from './'
 
 test.cb('throttle', t => {
   t.plan(1)
 
-  let counter = 0
-  const throttledIncrease = throttle(() => counter++)
-  throttledIncrease()
-  throttledIncrease()
+  const callbackSpy = spy()
 
-  setTimeout(() => {
-    t.is(counter, 1)
+  const throttled = throttle(callbackSpy)
+  throttled()
+  throttled()
+
+  raf(() => {
+    t.is(callbackSpy.callCount, 1)
     t.end()
-  }, 60)
+  })
+})
+
+test.cb('call the callback with arguments', t => {
+  t.plan(1)
+
+  const callbackSpy = spy()
+  const args = ['foo', 'bar']
+
+  const throttled = throttle(callbackSpy)
+  throttled(...args)
+
+  raf(() => {
+    t.same(callbackSpy.args[0], args)
+    t.end()
+  })
+})
+
+test.cb('more throttles', t => {
+  t.plan(1)
+
+  const callbackSpy = spy()
+
+  const throttled = throttle(callbackSpy)
+  throttled()
+  throttled()
+
+  raf(() => {
+    throttled()
+    throttled()
+
+    raf(() => {
+      t.is(callbackSpy.callCount, 2)
+      t.end()
+    })
+  })
 })
